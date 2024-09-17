5baca0cb1c6273262885b6d799cc2e5fe8c91a5e
Mamat1411 <muhammadmujahid14@gmail.com>
2024-09-17 13:47:02 +0700

    Day 01 Logic Phase

--- logic-phase/Day01.java
+++ logic-phase/Day01.java
@@ -1,5 +1,7 @@
 public class Day01 {
     public static void main(String[] args) {
+
+        // primitive data types
         boolean trueOrFalse = true;
         byte tinyNumber = 1;
         short smallNumber = 1;
@@ -9,45 +11,51 @@ public class Day01 {
         float mediumDecimal = 1.0f;
         double bigDecimal = 1.0;
 
+        // non-primitive data types
         Byte classTiny = 1;
         Short classSmall = 1;
         Integer classMedium = 1;
 
         Integer PascalCase = 1;
-        Integer camelCase = 516;
+        //variable declaration
+        Integer camelCase;
         Integer snake_case = 546;
 
-        // camelCase = snake_case;
+        // variable assignment
+        camelCase = snake_case;
 
-        // System.out.println(PascalCase);
+        System.out.println(PascalCase);
 
-        // System.out.println(classMedium); // 1
-        // ++classMedium;
-        // System.out.println(classMedium); // 2
+        // increment
+        System.out.println(classMedium); // 1
+        ++classMedium;
+        System.out.println(classMedium); // 2
 
-        // classMedium++;
-        // System.out.println(classMedium); // 3
-        // System.out.println();
+        classMedium++;
+        System.out.println(classMedium); // 3
+        System.out.println();
 
-        // for (int i = 0; i < 5; i++) {
-        //     System.out.print(classTiny++ + " ");
-        // }
+        for (int i = 0; i < 5; i++) {
+            System.out.print(classTiny++ + " ");
+        }
 
-        // System.err.println();
+        System.err.println();
 
-        // for (int i = 0; i < 5; i++) {
-        //     System.out.print(++classTiny + " ");
-        // }
+        for (int i = 0; i < 5; i++) {
+            System.out.print(++classTiny + " ");
+        }
 
-        // bigNumber += 5; // 6
-        // bigNumber += 5; // 11
+        // arithmatic compound assignment
+        bigNumber += 5; // 6
+        bigNumber += 5; // 11
 
-        // bigNumber = bigNumber + 5;
+        bigNumber = bigNumber + 5;
 
         String name = "Batch 342";
         String kelas = "Batch 342";
         String nama = "XSIS";
 
+        // control flow
         if (kelas.equals(name) || nama.contains("A")) {
             System.out.println("Yeay");
             if (bigNumber < 5 && bigDecimal != 0) {
@@ -60,6 +68,7 @@ public class Day01 {
             }
         }
 
+        // ternary operator
         String isTrue = kelas.equals(nama) || nama.contains("A") ? "Yeay" : bigNumber > 5 ? "Yeay" : "Naaah";
         System.out.println(isTrue);
     }
