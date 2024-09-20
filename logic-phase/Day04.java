8367f81c2c3d7418ada4c2a18b4d8be50ecbfe26
Mamat1411 <muhammadmujahid14@gmail.com>
2024-09-20 08:44:29 +0700

    starMatrix and indexMatrix Challenge

--- logic-phase/Day04.java
+++ logic-phase/Day04.java
@@ -1,5 +1,64 @@
+import java.util.Scanner;
+
 public class Day04 {
+
+    public static void matrixIndex(int size) {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                System.out.print("[" + i + "]" + "[" + j + "] ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void matrixSizable(int column, int row){
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < column; j++) {
+                System.out.print("[" + i + "]" + "[" + j + "] ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void starMatrix(int size) {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void sizableStarMatrix(int column, int row){
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < column; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
     public static void main(String[] args) {
+        /*
+         * 1. Dengan menggunakan scanner, Buatlah function untuk mencetak matrix dengan parameter sebagai size
+         *      matrix nya(kotak). munculkan index nya.
+         * 2. Dengan menggunakan scanner, Buatlah function untuk mencetak matrix dengan parameter column and row
+         *      sebagai ukuran matrix nya. munculkan index nya.
+         * 3. Sama seperti soal nomer 1 dan 2, tetapi cetak bintang(*)
+         */
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan Size Matrix: ");
+        int size = sc.nextInt();
+        matrixIndex(size);
+        System.out.println();
+        starMatrix(size);
+        System.out.println();
         
+        System.out.print("Masukkan Size Column Matrix: ");
+        int col = sc.nextInt();
+        System.out.print("Masukkan Size Row Matrix: ");
+        int row = sc.nextInt();
+        matrixSizable(col, row);
+        System.out.println();
+        sizableStarMatrix(col, row);
     }
 }
