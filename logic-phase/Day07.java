f542e596279441ce0c587f4abf4aa8f276e476b7
Mamat1411 <muhammadmujahid14@gmail.com>
2024-09-25 09:16:50 +0700

    Day 07: Try-Catch-Finally Block

--- logic-phase/Day07.java
+++ logic-phase/Day07.java
@@ -1,10 +1,24 @@
+import java.io.IOException;
+
 public class Day07 {
-    public static void main(String[] args) {
+    public static char readChar() throws IOException{
+        char c = '\u0000';
+        System.out.println(c);
+        int input = 0;
+        input = System.in.read();
+        if (input != -1) {
+            c = (char)input;
+        }
+        return c;
+    }
+    public static void main(String[] args) throws IOException, Day07Exception {
         int x = 10;
         int y = 0;
         int z;
-        // z = x / y;
-        // System.out.println("z = " + z); 
+
+        z = x / y;
+        System.out.println("z = " + z);
+
         try {
             z = x / y;
             System.out.println("z = " + z); 
@@ -12,6 +26,21 @@ public class Day07 {
             String msg = e.getMessage();
             System.out.println("An error has occured. The error is " + msg);
         }
-        System.out.println("End");
+
+        try {
+            z = x / y;
+            System.out.println("z = " + z);
+        } catch (ArithmeticException e) {
+            throw new Day07Exception("Nothing can be Divided By Zero");
+        }
+
+        try {
+            z = x / y;
+            System.out.println("z = " + z);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println("End");
+        }
     }
 }
