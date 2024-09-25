f3b4d6b56cfebd6b911844c6b60f2d25144265b5
Mamat1411 <muhammadmujahid14@gmail.com>
2024-09-25 09:44:08 +0700

    Variable Args

--- logic-phase/Day07.java
+++ logic-phase/Day07.java
@@ -1,46 +1,45 @@
-import java.io.IOException;
-
 public class Day07 {
-    public static char readChar() throws IOException{
-        char c = '\u0000';
-        System.out.println(c);
-        int input = 0;
-        input = System.in.read();
-        if (input != -1) {
-            c = (char)input;
+    
+    // variable arguments
+    public static int sum(int... values){
+        int result = 0;
+        for (int i = 0; i < values.length; i++) {
+            result += values[i];
         }
-        return c;
+        return result;
     }
-    public static void main(String[] args) throws IOException, Day07Exception {
-        int x = 10;
-        int y = 0;
-        int z;
+    public static void main(String[] args) throws Day07Exception {
+        // int x = 10;
+        // int y = 0;
+        // int z;
 
-        z = x / y;
-        System.out.println("z = " + z);
+        // z = x / y;
+        // System.out.println("z = " + z);
 
-        try {
-            z = x / y;
-            System.out.println("z = " + z); 
-        } catch (ArithmeticException e) {
-            String msg = e.getMessage();
-            System.out.println("An error has occured. The error is " + msg);
-        }
+        // try {
+        //     z = x / y;
+        //     System.out.println("z = " + z); 
+        // } catch (ArithmeticException e) {
+        //     String msg = e.getMessage();
+        //     System.out.println("An error has occured. The error is " + msg);
+        // }
 
-        try {
-            z = x / y;
-            System.out.println("z = " + z);
-        } catch (ArithmeticException e) {
-            throw new Day07Exception("Nothing can be Divided By Zero");
-        }
+        // try {
+        //     z = x / y;
+        //     System.out.println("z = " + z);
+        // } catch (ArithmeticException e) {
+        //     throw new Day07Exception("Nothing can be Divided By Zero");
+        // }
 
-        try {
-            z = x / y;
-            System.out.println("z = " + z);
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            System.out.println("End");
-        }
+        // try {
+        //     z = x / y;
+        //     System.out.println("z = " + z);
+        // } catch (Exception e) {
+        //     e.printStackTrace();
+        // } finally {
+        //     System.out.println("End");
+        // }
+
+        System.out.println(sum(1,2,3,4,5,6,7));
     }
 }
