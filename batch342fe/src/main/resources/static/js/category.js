23d0c591323b443395aa5164fb93831b3cb2cd42
Mamat1411 <muhammadmujahid14@gmail.com>
2024-10-23 13:58:48 +0700

    Stuck On Edit Caused By CORS

--- batch342fe/src/main/resources/static/js/category.js
+++ batch342fe/src/main/resources/static/js/category.js
@@ -28,7 +28,13 @@ function loadData() {
     });
 }
 
-function openForm() {
+function openForm(type) {
+    let button = ``;
+    if (type == "edit") {
+        button += `<button type="submit" class="btn btn-primary" style="float: right;" onclick="editCategory(this.value)">Update</button>`
+    } else {
+        button += `<button type="submit" class="btn btn-primary" style="float: right;" onclick="saveCategory()">Save</button>`
+    }
     $.ajax({
         type: "get",
         url: "/category/form",
@@ -37,6 +43,64 @@ function openForm() {
             $('#myModal').modal('show');
             $('.modal-title').html("Category Form");
             $('.modal-body').html(categoryForm);
+            $('#formButton').append(button);
+        }
+    });
+}
+
+function saveCategory() {
+    let jsonData = {
+        name: $('#categoryName').val(),
+        slug: $('#categorySlug').val(),
+        description: $("#categoryDesc").val()
+    }
+    $.ajax({
+        type: "POST",
+        url: "http://localhost:9001/api/category",
+        data: JSON.stringify(jsonData),
+        contentType: "application/json",
+        success: function (response) {
+            console.log(response);
+            location.reload();
+        },
+        error: function (error) {
+            console.error(error);
+        }
+    });
+}
+
+function editForm(id) {
+    openForm("edit");
+    $.ajax({
+        type: "get",
+        url: `http://localhost:9001/api/category/${id}`,
+        contentType: "application/json",
+        success: function (response) {
+            let categoryData = response.data;
+            $('#categoryName').val(categoryData.name);
+            $("#categorySlug").val(categoryData.slug);
+            $('#categoryDesc').val(categoryData.description);
+        }
+    });
+}
+
+function editCategory(id) {
+    let jsonData = {
+        name: $('#categoryName').val(),
+        slug: $("#categorySlug").val(),
+        description: $('#categoryDesc').val()
+    }
+    $.ajax({
+        url: `http://localhost:9001/api/category/${id}`,
+        type: "PUT",
+        header: {
+            "Access-Control-Allow-Origin" : `*`,
+        },
+        data: JSON.stringify(jsonData),
+        contentType: "application/json",
+        success: function (response) {
+            console.log(response);
+            location.reload();
         }
     });
 }
