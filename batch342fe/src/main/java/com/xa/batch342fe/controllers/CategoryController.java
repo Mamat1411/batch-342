d721a708cd090187f4933a25d0535424dd913ee0
Mamat1411 <muhammadmujahid14@gmail.com>
2024-10-23 11:09:37 +0700

    Category Frontend In Progress

---
+++ batch342fe/src/main/java/com/xa/batch342fe/controllers/CategoryController.java
@@ -0,0 +1,27 @@
+package com.xa.batch342fe.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+
+@Controller
+@RequestMapping("/category")
+public class CategoryController {
+    
+    @GetMapping("")
+    public ModelAndView category() {
+        ModelAndView view = new ModelAndView("category/index");
+        view.addObject("title", "Category Page");
+        return view;
+    }
+    
+    @GetMapping("/form")
+    public ModelAndView form() {
+        ModelAndView view = new ModelAndView();
+        return view;
+    }
+    
+}
--- batch342fe/src/main/resources/static/js/category.js
+++ batch342fe/src/main/resources/static/js/category.js
@@ -1,3 +1,33 @@
+loadData();
+function loadData() {
+    let tableData = ``;
+    $.ajax({
+        type: "get",
+        url: "http://localhost:9001/api/category",
+        contentType: "application/json",
+        success: function (categoryResponse) {
+            let categoryData = categoryResponse.data;
+
+            categoryData.forEach((category, index) => {
+                tableData += `
+                  <tr>
+                    <td>${index+1}</td>
+                    <td>${category.name}</td>
+                    <td>${category.slug}</td>
+                    <td>${category.description}</td>
+                    <td>
+                        <button class="btn btn-warning" onclick="editForm(${category.id})">Edit</button>
+                        <button class="btn btn-danger" onclick="deleteForm(${category.id})">Delete</button>
+                    </td>
+                  </tr>
+                `;
+            });
+
+            $('#categoryTable').html(tableData);
+        }
+    });
+}
+
 function openForm() {
     $.ajax({
         type: "get",
@@ -7,43 +37,6 @@ function openForm() {
             $('#myModal').modal('show');
             $('.modal-title').html("Category Form");
             $('.modal-body').html(categoryForm);
-        }
-    });
-}
-
-function editForm(id) {
-    $.ajax({
-        type: "get",
-        url: `/category/edit/${id}`,
-        contentType: `html`,
-        success: function (categoryForm) {
-            $('#myModal').modal('show');
-            $('.modal-title').html("Category Form");
-            $('.modal-body').html(categoryForm);
-        }
-    });
-}
-
-function deleteForm(id) {
-    $.ajax({
-        type: "get",
-        url: `/category/deleteForm/${id}`,
-        contentType: `html`,
-        success: function (categoryForm) {
-            $('#myModal').modal('show');
-            $('.modal-title').html("Category Form");
-            $('.modal-body').html(categoryForm);
-        }
-    });
-}
-
-function deleteCategory(id) {
-    $.ajax({
-        type: "get",
-        url: `/category/delete/${id}`,
-        contentType: `html`,
-        success: function (response) {
-            location.reload();
         }
     });
 }
